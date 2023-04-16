a2055994182349c5cb6720f5c6090e3874d1bfb7
Joldasbaev Salawat <salawat.me@Joldasbaevs-MacBook-Pro.local>
2023-04-16 19:02:58 +0500

    service

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Category\CategoryResource;
+use App\Models\Category;
+use App\Services\Category\StoreCategory;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $category = app(StoreCategory::class)->execute($request->all());
+            return new CategoryResource($category);
+        }catch (ValidationException $exception){
+            return $exception->validator->errors()->all();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @method static create(array $data)
+ */
 class Category extends Model
 {
     use HasFactory;
@@ -14,7 +17,7 @@ class Category extends Model
         'name',
     ];
 
-    public $timestamps = false;
+    public $timestamps = true;
 
     public function collections(): HasMany
     {
